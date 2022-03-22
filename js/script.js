dab3ee4e910647573e8b17d8007b080158163919
Sandra Pater <sandra.pater@gmail.com>
2022-03-22 11:48:27 +0100

    adding new task

--- js/script.js
+++ js/script.js
@@ -18,17 +18,41 @@
             htmlString += `
             <li>
                 ${task.content}
-            </li >
+            </li>
             `;
-
-            document.querySelector(".js-tasks").innerHTML = htmlString;
         }
 
-    }
-    const init = () => {
-        render();
-    }
+            document.querySelector(".js-tasks").innerHTML = htmlString;
+        };
 
-    init();
+        const addNewTask = (newTaskContent) => {
+            tasks.push({
+                content: newTaskContent,
 
-}
+
+            });
+
+            render();
+        };
+
+        const init = () => {
+            render();
+
+            const form = document.querySelector(".js-form");
+
+            form.addEventListener("submit", (event) => {
+                event.preventDefault();
+
+                const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+                if (newTaskContent === "") {
+                    return;
+                }
+
+                addNewTask(newTaskContent);
+            });
+        };
+
+        init();
+
+    }
